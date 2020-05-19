79f72fb67323abbeb9dc78adc07d94e236381d2c
Abbos-coder <63785703+Abbos-coder@users.noreply.github.com>
2020-05-19 21:04:37 +0500

    Add files via upload

---
+++ jQuery/jQuery.js
@@ -0,0 +1,540 @@
+jQuery(document).ready(function () {
+   let img = $("#1");
+   let btns = $("#11");
+
+   btns.css({
+      opacity: "0"
+   });
+   img.mouseenter(function () {
+      btns.css({
+         opacity: "1"
+      }, 4000);
+      img.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img.mouseleave(function () {
+      btns.css({
+         opacity: "0"
+      }, 1000);
+      img.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns.mouseenter(function () {
+      btns.css({
+         opacity: "1"
+      }, 1000);
+      img.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns.mouseleave(function () {
+      btns.css({
+         opacity: "0"
+      }, 1000);
+      img.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   //    two
+   var btns22 = $("#22");
+   var img2 = $("#2");
+
+   btns22.css({
+      opacity: "0"
+   });
+   img2.mouseenter(function () {
+      btns22.css({
+         opacity: "1"
+      }, 4000);
+      img2.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img2.mouseleave(function () {
+      btns22.css({
+         opacity: "0"
+      }, 1000);
+      img2.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns22.mouseenter(function () {
+      btns22.css({
+         opacity: "1"
+      }, 1000);
+      img2.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns22.mouseleave(function () {
+      btns22.css({
+         opacity: "0"
+      }, 1000);
+      img2.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   //    tree
+   var btns33 = $("#33");
+   var img3 = $("#3");
+
+   btns33.css({
+      opacity: "0"
+   });
+   img3.mouseenter(function () {
+      btns33.css({
+         opacity: "1"
+      }, 4000);
+      img3.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img3.mouseleave(function () {
+      btns33.css({
+         opacity: "0"
+      }, 1000);
+      img3.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns33.mouseenter(function () {
+      btns33.css({
+         opacity: "1"
+      }, 1000);
+      img3.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns33.mouseleave(function () {
+      btns33.css({
+         opacity: "0"
+      }, 1000);
+      img3.css({
+         filter: "blur(0px)"
+      });
+   });
+   //    four
+   var btns44 = $("#44");
+   var img4 = $("#4");
+
+   btns44.css({
+      opacity: "0"
+   });
+   img4.mouseenter(function () {
+      btns44.css({
+         opacity: "1"
+      }, 4000);
+      img4.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img4.mouseleave(function () {
+      btns44.css({
+         opacity: "0"
+      }, 1000);
+      img4.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns44.mouseenter(function () {
+      btns44.css({
+         opacity: "1"
+      }, 1000);
+      img4.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns44.mouseleave(function () {
+      btns44.css({
+         opacity: "0"
+      }, 1000);
+      img4.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   //    five
+
+   var btns55 = $("#55");
+   var img5 = $("#5");
+
+   btns55.css({
+      opacity: "0"
+   });
+   img5.mouseenter(function () {
+      btns55.css({
+         opacity: "1"
+      }, 4000);
+      img5.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img5.mouseleave(function () {
+      btns55.css({
+         opacity: "0"
+      }, 1000);
+      img5.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns55.mouseenter(function () {
+      btns55.css({
+         opacity: "1"
+      }, 1000);
+      img5.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns55.mouseleave(function () {
+      btns55.css({
+         opacity: "0"
+      }, 1000);
+      img5.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   //    six
+
+   var btns66 = $("#66");
+   var img6 = $("#6");
+
+   btns66.css({
+      opacity: "0"
+   });
+   img6.mouseenter(function () {
+      btns66.css({
+         opacity: "1"
+      }, 4000);
+      img6.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img6.mouseleave(function () {
+      btns66.css({
+         opacity: "0"
+      }, 1000);
+      img6.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns66.mouseenter(function () {
+      btns66.css({
+         opacity: "1"
+      }, 1000);
+      img6.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns66.mouseleave(function () {
+      btns66.css({
+         opacity: "0"
+      }, 1000);
+      img6.css({
+         filter: "blur(0px)"
+      });
+   });
+   //    seven
+
+   var btns77 = $("#77");
+   var img7 = $("#7");
+
+   btns77.css({
+      opacity: "0"
+   });
+   img7.mouseenter(function () {
+      btns77.css({
+         opacity: "1"
+      }, 4000);
+      img7.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img7.mouseleave(function () {
+      btns77.css({
+         opacity: "0"
+      }, 1000);
+      img7.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns77.mouseenter(function () {
+      btns77.css({
+         opacity: "1"
+      }, 1000);
+      img7.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns77.mouseleave(function () {
+      btns77.css({
+         opacity: "0"
+      }, 1000);
+      img7.css({
+         filter: "blur(0px)"
+      });
+   });
+   //    eight
+   var btns88 = $("#88");
+   var img8 = $("#8");
+
+   btns88.css({
+      opacity: "0"
+   });
+   img8.mouseenter(function () {
+      btns88.css({
+         opacity: "1"
+      }, 4000);
+      img8.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   img8.mouseleave(function () {
+      btns88.css({
+         opacity: "0"
+      }, 1000);
+      img8.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   btns88.mouseenter(function () {
+      btns88.css({
+         opacity: "1"
+      }, 1000);
+      img8.css({
+         filter: "blur(3px)"
+      });
+   });
+
+   btns88.mouseleave(function () {
+      btns88.css({
+         opacity: "0"
+      }, 1000);
+      img8.css({
+         filter: "blur(0px)"
+      });
+   });
+
+   //    a and aa
+
+   var btnsAA = $("#aa");
+   var imgA = $("#a");
+
+   btnsAA.css({
+      opacity: "0"
+   });
+
+   imgA.mouseenter(function () {
+      btnsAA.css({
+         opacity: "1"
+      }, 4000);
+      imgA.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   imgA.mouseleave(function () {
+      btnsAA.css({
+         opacity: "0"
+      }, 1000);
+      imgA.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+
+   btnsAA.mouseenter(function () {
+      btnsAA.css({
+         opacity: "1"
+      }, 1000);
+      imgA.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   btnsAA.mouseleave(function () {
+      btnsAA.css({
+         opacity: "0"
+      }, 1000);
+      imgA.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+   // bb
+   var btnsBB = $("#bb");
+   var imgB = $("#b");
+
+   btnsBB.css({
+      opacity: "0"
+   });
+
+   imgB.mouseenter(function () {
+      btnsBB.css({
+         opacity: "1"
+      }, 4000);
+      imgB.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   imgB.mouseleave(function () {
+      btnsBB.css({
+         opacity: "0"
+      }, 1000);
+      imgB.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+
+   btnsBB.mouseenter(function () {
+      btnsBB.css({
+         opacity: "1"
+      }, 1000);
+      imgB.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   btnsBB.mouseleave(function () {
+      btnsBB.css({
+         opacity: "0"
+      }, 1000);
+      imgB.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+   // cc
+
+   var btnsCC = $("#cc");
+   var imgC = $("#c");
+
+   btnsCC.css({
+      opacity: "0"
+   });
+
+   imgC.mouseenter(function () {
+      btnsCC.css({
+         opacity: "1"
+      }, 4000);
+      imgC.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   imgC.mouseleave(function () {
+      btnsCC.css({
+         opacity: "0"
+      }, 1000);
+      imgC.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+
+   btnsCC.mouseenter(function () {
+      btnsCC.css({
+         opacity: "1"
+      }, 1000);
+      imgC.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   btnsCC.mouseleave(function () {
+      btnsCC.css({
+         opacity: "0"
+      }, 1000);
+      imgC.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+
+   //    dd
+
+   var btnsDD = $("#dd");
+   var imgD = $("#d");
+
+   btnsDD.css({
+      opacity: "0"
+   });
+
+   imgD.mouseenter(function () {
+      btnsDD.css({
+         opacity: "1"
+      }, 4000);
+      imgD.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   imgD.mouseleave(function () {
+      btnsDD.css({
+         opacity: "0"
+      }, 1000);
+      imgD.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+
+   btnsDD.mouseenter(function () {
+      btnsDD.css({
+         opacity: "1"
+      }, 1000);
+      imgD.css({
+         filter: "blur(3px)"
+      }, 400);
+   });
+
+   btnsDD.mouseleave(function () {
+      btnsDD.css({
+         opacity: "0"
+      }, 1000);
+      imgD.css({
+         filter: "blur(0px)"
+      }, 400);
+   });
+   // burger 
+   let burger = $('.burger');
+   let show = $('#show');
+   let hide = $('#hide');
+   let menu = $('ul li a');
+
+   // burger.fadeOut();
+
+   show.on('click', function () {
+      burger.slideDown(400);
+   });
+
+   hide.on('click', function () {
+      burger.slideUp(300);
+   });
+
+   menu.on('click', function () {
+      burger.slideUp(300);
+   });
+});
